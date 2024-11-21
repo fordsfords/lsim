f792e1477c8cc6509709e5b4a5b72a06b1994b5b
Steve Ford <sford@geeky-boy.com>
2024-11-21 18:01:17 -0500

    updates

--- cfg.h
+++ cfg.h
@@ -1,4 +1,4 @@
-/* err.h - simple error handler. */
+/* cfg.h - simple error handler. */
 /*
 # This code and its documentation is Copyright 2024-2024 Steven Ford, http://geeky-boy.com
 # and licensed "public domain" style under Creative Commons "CC0": http://creativecommons.org/publicdomain/zero/1.0/
@@ -6,7 +6,7 @@
 # waived all copyright and related or neighboring rights to this work.
 # In other words, you can use this code for any purpose without any
 # restrictions.  This work is published from: United States.  The project home
-# is https://github.com/fordsfords/lsim
+# is https://github.com/fordsfords/cfg
 */
 
 #ifndef CFG_H
